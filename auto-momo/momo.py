ab9cac590852b884820cb552cb7d7657dde63eb1
ccccc <260374918@qq.com>
2024-08-24 21:14:41 +0800

    Update momo.py

--- auto-momo/momo.py
+++ auto-momo/momo.py
@@ -5,7 +5,7 @@ from asyncio import create_task, wait, Semaphore, run
 from aiohttp import ClientSession, ClientTimeout
 
 global n  # 记录访问成功次数
-link = 'https://www.maimemo.com/page?sid=7a9e30670b28730d0ebb5219dedfb4eb&uid=19253996&pid=2cfaa018e3403c2daadb212e3a941b49&tid=52e66ba0ff3fc8263abdb6661f966a0b&view_time=1724504670&bind_user=0&create_user=0&oauth=wechat_wx6a4680fa1ef1b496&unionid=o6IDit3i0oc7OvR85vMQFTdWB0CE&token=580857d6dca812bed7391ac883d525dc506aa4a49406f23ab67fda093123df85&expired_time=2025-08-24T21:04:30%2008:00'  # 设置link
+link = 'link'  # 设置link
 
 # 如果检测到程序在 github actions 内运行，那么读取环境变量中的登录信息
 if environ.get('GITHUB_RUN_ID', None):
